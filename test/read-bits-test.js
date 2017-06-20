dfb386f6819c9c8c84c0d94ff4057f6d1a82c699
Aaron Boushley <boushley@gmail.com>
2017-06-19 21:39:40 -0700

    Get readBits functional.

--- test/read-bits-test.js
+++ test/read-bits-test.js
@@ -10,26 +10,49 @@ describe('readBits', function() {
 /*20*/0x04, 0x05, 0x06, 0x07, 0x08
     ])
 
-  describe('a single bit without bit offsets', function() {
+  describe('without a bit offset', function() {
     it('should read some bits', function() {
       const result = pp.readBits(buffer, 1, {lengthBits: 5}, 0)
-      assert.equal(0x1f, result[0])
+      assert.equal(result[0], 0x1f)
+      assert.equal(result.length, 1)
     })
-    it('should read some bits when not set', function() {
+    it('should read some bits regardless of their value', function() {
       const result = pp.readBits(buffer, 15, {lengthBits: 5}, 0)
-      assert.equal(0x1e, result[0])
+      assert.equal(result[0], 0x1e)
+      assert.equal(result.length, 1)
+    })
+
+    it('should read multiple bytes', function() {
+      const result = pp.readBits(buffer, 0, {lengthBits: 23}, 0)
+      // from bits:   1111 1111 1111 1110 1111 1101
+      // result bits: 0111 1111 1111 1111 0111 1110
+      assert.equal(result[0], 0x7f)
+      assert.equal(result[1], 0xff)
+      assert.equal(result[2], 0x7e)
+      assert.equal(result.length, 3)
     })
   })
 
-  describe('a couple bytes without bit offsets', function() {
-    it('should read some bits', function() {
-      const result = pp.readBits(buffer, 0, {lengthBits: 23}, 0)
-      console.log(result)
-      // from bits:   1111 1111 1111 1110 1111 1101
-      // result bits: 0111 1111 1111 1111 0111 1110
-      assert.equal(0x7f, result[0])
-      assert.equal(0xff, result[1])
-      assert.equal(0x7e, result[2])
+  describe('with bit offset', function() {
+    it('should read multiple bytes', function() {
+      const result = pp.readBits(buffer, 1, {lengthBits: 22}, 4)
+      //                   |-- should select these --|
+      // from bits:   1111 1110 1111 1101 1111 1100 1111 1011
+      // result bits: 0011 1011 1111 0111 1111 0011
+      assert.equal(result[0], 0x3b)
+      assert.equal(result[1], 0xf7)
+      assert.equal(result[2], 0xf3)
+      assert.equal(result.length, 3)
+    })
+    it('should read multiple different bytes', function() {
+      const result = pp.readBits(buffer, 16, {lengthBits: 23}, 1)
+      //               |-- should select these  --|
+      // from bits:   0000 0000 0000 0001 0000 0010
+      // result bits: 0000 0000 0000 0001 0000 0010
+      assert.equal(result[0], 0x00)
+      assert.equal(result[1], 0x01)
+      assert.equal(result[2], 0x02)
+      assert.equal(result.length, 3)
     })
   })
 
